f975d12b1cd0f78ebbe726e6c84947ad4df0c069
SKRohit <rohitku.singh8@gmail.com>
2021-09-10 03:23:34 +0530

    run without aai

--- cv/app.py
+++ cv/app.py
@@ -1,19 +1,10 @@
 import time
-import edgeiq
+from imutils.video.pivideostream import PiVideoStream
+import imutils
 import argparse
 import socketio
 import cv2
 import base64
-"""
-Use object detection to detect objects in the frame in realtime. The
-types of objects detected can be changed by selecting different models.
-
-To change the computer vision model, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
-
-To change the engine and accelerator, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_and_accelerator.html
-"""
 
 sio = socketio.Client()
 
@@ -24,7 +15,7 @@ def connect():
 
 
 @sio.event
-def connect_error():
+def connect_error(p):
     print('[INFO] Failed to connect to server.')
 
 
@@ -63,8 +54,9 @@ class CVClient(object):
         cur_t = time.time()
         if cur_t - self._last_update_t > self._wait_t:
             self._last_update_t = cur_t
-            frame = edgeiq.resize(
-                    frame, width=640, height=480, keep_scale=True)
+            # frame = edgeiq.resize(
+            #         frame, width=640, height=480, keep_scale=True)
+            #print('frame_szie',frame.shape)
             sio.emit(
                     'cv2server',
                     {
@@ -80,59 +72,31 @@ class CVClient(object):
 
 
 def main(camera, use_streamer, server_addr, stream_fps):
-    obj_detect = edgeiq.ObjectDetection(
-            "alwaysai/mobilenet_ssd")
-    obj_detect.load(engine=edgeiq.Engine.DNN)
 
-    print("Loaded model:\n{}\n".format(obj_detect.model_id))
-    print("Engine: {}".format(obj_detect.engine))
-    print("Accelerator: {}\n".format(obj_detect.accelerator))
-    print("Labels:\n{}\n".format(obj_detect.labels))
-
-    fps = edgeiq.FPS()
+    # fps = edgeiq.FPS()
 
     try:
         streamer = None
-        if use_streamer:
-            streamer = edgeiq.Streamer().setup()
-        else:
-            streamer = CVClient(server_addr, stream_fps).setup()
+        streamer = CVClient(server_addr, stream_fps).setup()
 
-        with edgeiq.WebcamVideoStream(cam=camera) as video_stream:
-            # Allow Webcam to warm up
-            time.sleep(2.0)
-            fps.start()
+        #video_stream = PiVideoStream().start()
+        video_stream = cv2.VideoCapture(0)
 
-            # loop detection
-            while True:
-                frame = video_stream.read()
-                results = obj_detect.detect_objects(frame, confidence_level=.5)
-                frame = edgeiq.markup_image(
-                        frame, results.predictions, colors=obj_detect.colors)
+        while True:
+            frame = video_stream.read()[1]
+            frame = imutils.resize(frame, width=200, height=200)
+            #print('printing there',type(frame),frame)
+            # Generate text to display on streamer
+            text = []
+            text.append("Objects:")
+            streamer.send_data(frame, text)
 
-                # Generate text to display on streamer
-                text = ["Model: {}".format(obj_detect.model_id)]
-                text.append(
-                        "Inference time: {:1.3f} s".format(results.duration))
-                text.append("Objects:")
-
-                for prediction in results.predictions:
-                    text.append("{}: {:2.2f}%".format(
-                        prediction.label, prediction.confidence * 100))
-
-                streamer.send_data(frame, text)
-
-                fps.update()
-
-                if streamer.check_exit():
-                    break
+            if streamer.check_exit():
+                break
 
     finally:
         if streamer is not None:
             streamer.close()
-        fps.stop()
-        print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
-        print("approx. FPS: {:.2f}".format(fps.compute_fps()))
 
         print("Program Ending")
 
